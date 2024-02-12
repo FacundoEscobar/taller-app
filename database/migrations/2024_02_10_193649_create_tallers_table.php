801a599fa1f8a19d44bc280006abe2e7dbdcd7a9
Facundo Escobar <efacundo1808@gmail.com>
2024-02-12 17:35:16 -0300

    comienzo del proyecto

---
+++ database/migrations/2024_02_10_193649_create_tallers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tallers', function (Blueprint $table) {
+            $table->string('title', 100);
+            $table->string('language', 20);
+            $table->text('code');
+            $table->json('tags');
+            
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tallers');
+    }
+};
